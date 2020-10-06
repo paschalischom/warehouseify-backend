e1caae05a5dda635e77a8a869af94ab21167944b
paschalishom <paschalhom@gmail.com>
2020-10-06 03:33:29 +0300

    Added initial query ranking capabilities.

--- src/main/java/com/uoi/spmsearch/service/GoogleMapsService.java
+++ src/main/java/com/uoi/spmsearch/service/GoogleMapsService.java
@@ -47,7 +47,7 @@ public class GoogleMapsService {
         String encodedAddress = URLEncoder.encode(addressRequest.getAddress(), StandardCharsets.UTF_8);
         String urlString = "https://maps.googleapis.com/maps/api/geocode/json?" +
                 "language=en&region=us&address=" + encodedAddress +
-                "&key=AIzaSyD-qKqWPwaGLYgulj0LjrIitRlTy98YgIs";
+                "&key=<GEOCODING_API_KEY>";
         String response = executeRequest(urlString);
         return new ObjectMapper().readValue(response, GoogleMapsForwardGeocodingResponseResult.class);
     }
@@ -60,7 +60,7 @@ public class GoogleMapsService {
         String encodedLatLng = locationRequest.encodeReverseGeocodingUrl();
         String urlString = "https://maps.googleapis.com/maps/api/geocode/json?" +
                 "language=en&result_type=street_address&latlng=" + encodedLatLng +
-                "&key=AIzaSyD-qKqWPwaGLYgulj0LjrIitRlTy98YgIs";
+                "&key=<GEOCODING_API_KEY>";
         String response = executeRequest(urlString);
         return new ObjectMapper().readValue(response, GoogleMapsReverseGeocodingResponseResult.class);
     }
@@ -72,7 +72,7 @@ public class GoogleMapsService {
             urlString.append(destination.getLatitude()).append(",").append(destination.getLongitude()).append("|");
         }
         urlString.setLength(urlString.length() - 1);
-        urlString.append("&key=AIzaSyAnBlrwZSLLF3BKkpBfWofB1u8CaXZW93w");
+        urlString.append("&key=<DISTANCE_MATRIX_API_KEY>");
         //System.out.printf("Url: %s\n", urlString.toString());
         String response = executeRequest(urlString.toString());
         //System.out.println(response);
